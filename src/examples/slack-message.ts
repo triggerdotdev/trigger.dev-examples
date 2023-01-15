b5ddd3a6f1e10c9d7fbad0f7059ff3c06a007bd1
James Ritchie <james@jamesritchie.co.uk>
2023-01-15 10:53:19 -0800

    Updated the slack example code

--- src/examples/slack-message.ts
+++ src/examples/slack-message.ts
@@ -2,26 +2,33 @@ import { Trigger, customEvent } from "@trigger.dev/sdk";
 import { slack } from "@trigger.dev/integrations";
 import { z } from "zod";
 
-export const postMessage = new Trigger({
-  id: "slack-post-messages",
-  name: "Post to slack",
-  logLevel: "debug",
+const postMessage = new Trigger({
+  id: "new-user",
+  name: "New user slack message",
+  apiKey: "trigger_development_hOOzMfDV2G04",
+  logLevel: "info",
   on: customEvent({
-    name: "slack-message",
+    name: "user.created",
     schema: z.object({
-      message: z.string(),
+      name: z.string(),
+      email: z.string(),
+      paidPlan: z.boolean(),
     }),
   }),
   run: async (event, ctx) => {
     await ctx.logger.info("This log will appear on the Trigger.dev run page");
 
-    //send a message to the #test-integrations Slack channel
-    //it uses the message text that was passed in the event
+    //send a message to the #new-users Slack channel with user details
     const response = await slack.postMessage("send-to-slack", {
       channel: "test-integrations",
-      text: event.message,
+      text: `New user: ${event.name} (${event.email}) signed up. ${
+        event.paidPlan ? "They are paying" : "They are on the free plan"
+      }.`,
     });
 
     return response.message;
   },
 });
+
+//this workflow will now connect and start listening for events
+postMessage.listen();
