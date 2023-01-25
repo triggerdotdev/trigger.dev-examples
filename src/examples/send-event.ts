a6c88ec178b6335af8e6f930c334cb2cac7e9233
Matt Aitken <matt@mattaitken.com>
2023-01-25 15:47:49 -0800

    Send event examples, triggered from outside workflow and then triggers another workflow

--- src/examples/send-event.ts
+++ src/examples/send-event.ts
@@ -8,7 +8,7 @@ import { z } from "zod";
 
 new Trigger({
   id: "important-workflow",
-  name: "A very important workflow, with custom event",
+  name: "A very important workflow, that sends a custom event",
   apiKey: "trigger_development_mLpn9DEEqCRR",
   on: customEvent({
     name: "important.event",
@@ -18,27 +18,32 @@ new Trigger({
   }),
   async run(event, context) {
     await sendEvent("start-other-workflow", {
-      name: "sendevent.test",
+      name: "sent.from.workflow",
       payload: {
-        message: "You can easily trigger workflows from other workflows",
+        message:
+          "You can easily trigger workflows from other workflows, like this",
       },
     });
-    return {};
+    return;
   },
 });
 
 new Trigger({
-  id: "send-an-event",
-  name: "Send an event to start a different workflow",
+  id: "receive-from-other-workflow",
+  name: "Receive an event from a different workflow",
   apiKey: "trigger_development_mLpn9DEEqCRR",
-  on: scheduleEvent({}),
+  on: customEvent({
+    name: "sent.from.workflow",
+    schema: z.object({
+      message: z.string(),
+    }),
+  }),
   async run(event, context) {
-    await sendEvent("start-other-workflow", {
-      name: "important.event",
-      payload: {
-        message: "You can easily trigger workflows from other workflows",
-      },
-    });
-    return {};
+    return;
   },
+});
+
+sendEvent("fire.important.event", {
+  name: "important.event",
+  payload: { message: "This was triggered from a custom event in code" },
 });
