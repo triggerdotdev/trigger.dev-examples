d37fad06ca0037a5bce39ea7948ee66cf419b96b
Matt Aitken <matt@mattaitken.com>
2023-01-28 18:31:02 -0800

    Added slack blocks example and healthcheck example

---
+++ src/examples/healthcheck.tsx
@@ -0,0 +1,98 @@
+/** @jsxImportSource jsx-slack */
+import { slack } from "@trigger.dev/integrations";
+import { fetch, scheduleEvent, Trigger } from "@trigger.dev/sdk";
+import dotenv from "dotenv";
+import JSXSlack, { Actions, Blocks, Button, Section } from "jsx-slack";
+dotenv.config();
+
+const sites = [
+  { url: "https://www.google.com", email: "matt@trigger.dev" },
+  { url: "https://www.sitedoesntexist.co", email: "matt@trigger.dev" },
+  { url: "https://www.facebook.com", email: "matt@trigger.dev" },
+  { url: "https://www.amazon.com", email: "matt@trigger.dev" },
+];
+
+const BLOCK_ID = "action.buttons";
+
+new Trigger({
+  id: "scheduled-healthcheck",
+  name: "Scheduled healthcheck",
+  endpoint: "ws://localhost:8889/ws",
+  logLevel: "info",
+  on: scheduleEvent({
+    rateOf: {
+      minutes: 1,
+    },
+  }),
+  run: async (event, ctx) => {
+    const promises = sites.map(async (site) => {
+      const up = await isSiteUp(site.url);
+
+      if (!up) {
+        await slack.postMessage(`${site}-down-message`, {
+          channelName: "test-integrations",
+          text: "How is your progress today?",
+          blocks: JSXSlack(
+            <Blocks>
+              <Section>⛔️ {site.url} is down</Section>
+              <Actions blockId={BLOCK_ID}>
+                <Button actionId="retry" value={site.url}>
+                  Retry
+                </Button>
+                <Button url="https://xkcd.com/1349/">Dashboard</Button>
+                <Button url={`mailto:${site.email}`}>Email customer</Button>
+              </Actions>
+            </Blocks>
+          ),
+        });
+      }
+    });
+
+    await Promise.all(promises);
+    return {};
+  },
+}).listen();
+
+//listen for pressed of the retry button
+new Trigger({
+  id: "healthcheck-retry-interaction",
+  name: "Healthcheck retry interaction",
+  endpoint: "ws://localhost:8889/ws",
+  on: slack.events.blockActionInteraction({
+    blockId: BLOCK_ID,
+    actionId: ["retry"],
+  }),
+  run: async (event, ctx) => {
+    const firstAction = event.actions[0];
+    if (!firstAction) return;
+    if (!event.message) return;
+
+    if (firstAction.action_id !== "retry") {
+      ctx.logger.error(`Unexpected action: ${event.actions[0]?.action_id}`);
+      return;
+    }
+
+    if (firstAction.type === "button") {
+      const siteUrl = firstAction.value;
+      const up = await isSiteUp(siteUrl);
+      await slack.postMessageResponse("React to message", event.response_url, {
+        text: up ? `✅ ${siteUrl} is now up!` : `😭 ${siteUrl} is still down`,
+        replace_original: false,
+      });
+    }
+  },
+}).listen();
+
+async function isSiteUp(url: string) {
+  try {
+    const response = await fetch(`healthcheck-${url}`, url, {
+      retry: {
+        enabled: false,
+      },
+    });
+
+    return response.ok;
+  } catch (e) {
+    return false;
+  }
+}
---
+++ src/examples/slack-blocks.tsx
@@ -0,0 +1,117 @@
+/** @jsxImportSource jsx-slack */
+import { slack } from "@trigger.dev/integrations";
+import { scheduleEvent, Trigger } from "@trigger.dev/sdk";
+import JSXSlack, {
+  Actions,
+  Blocks,
+  Button,
+  Section,
+  Select,
+  Option,
+} from "jsx-slack";
+import dotenv from "dotenv";
+dotenv.config();
+
+const BLOCK_ID = "issue.action.block";
+
+//1. every minute see how your employees are doing, we don't recommend this frequency 😉
+new Trigger({
+  id: "slack-interactivity",
+  name: "Testing Slack Interactivity",
+  on: scheduleEvent({
+    rateOf: {
+      minutes: 1,
+    },
+  }),
+  run: async (event, ctx) => {
+    await slack.postMessage("jsx-test", {
+      channelName: "test-integrations",
+      //text appears in Slack notifications on mobile/desktop
+      text: "How is your progress today?",
+      //import and use JSXSlack to make creating rich messages much easier
+      blocks: JSXSlack(
+        <Blocks>
+          <Section>How is your progress today?</Section>
+          <Actions blockId={BLOCK_ID}>
+            <Button value="blocked" actionId="status-blocked">
+              I'm blocked
+            </Button>
+            <Button
+              value="help"
+              actionId="status-help"
+              url="https://xkcd.com/1349/"
+            >
+              Get help
+            </Button>
+            <Select actionId="rating" placeholder="Rate it!">
+              <Option value="5">5 {":star:".repeat(5)}</Option>
+              <Option value="4">4 {":star:".repeat(4)}</Option>
+              <Option value="3">3 {":star:".repeat(3)}</Option>
+              <Option value="2">2 {":star:".repeat(2)}</Option>
+              <Option value="1">1 {":star:".repeat(1)}</Option>
+            </Select>
+          </Actions>
+        </Blocks>
+      ),
+    });
+  },
+}).listen();
+
+//2. this workflow listens for Slack interactions filtered by the block id and actions we used above
+new Trigger({
+  id: "slack-block-interaction",
+  name: "Slack Block Interaction",
+  on: slack.events.blockActionInteraction({
+    blockId: BLOCK_ID,
+    actionId: ["status-blocked", "status-help", "rating"],
+  }),
+  run: async (event, ctx) => {
+    //create promises from all the actions
+    const promises = event.actions.map((action) => {
+      switch (action.action_id) {
+        case "status-blocked": {
+          //the user is blocked so add a 😢 emoji as a reaction
+          if (event.message) {
+            return slack.addReaction("React to message", {
+              name: "cry",
+              timestamp: event.message.ts,
+              channelId: event.channel.id,
+            });
+          }
+          break;
+        }
+        case "status-help": {
+          //the user needs help so add an 🆘 emoji as a reaction
+          if (event.message) {
+            return slack.addReaction("React to message", {
+              name: "sos",
+              timestamp: event.message.ts,
+              channelId: event.channel.id,
+            });
+          }
+          break;
+        }
+        case "rating": {
+          if (action.type != "static_select") {
+            throw new Error("This action should be a select");
+          }
+
+          //post the rating as a message that appears below the original,
+          //only the user pressing the button will see this message
+          return slack.postMessageResponse(
+            "Added a comment to the issue",
+            event.response_url,
+            {
+              text: `You rated your day ${action.selected_option?.value} stars`,
+              replace_original: false,
+            }
+          );
+        }
+        default:
+          return Promise.resolve();
+      }
+    });
+
+    await Promise.all(promises);
+  },
+}).listen();
